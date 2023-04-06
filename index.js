428e3c6d2f698aa5a5afc32dc208e4b37cb86081
John Kluse <j.kluse@gmail.com>
2023-04-06 12:27:06 -0600

    Completed Stoplight

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
-(function() {
-  'use strict';
+let stopButton = document.getElementById("stopButton");
+let stopLight = document.getElementById("stopLight");
 
-  // YOUR CODE HERE
-})();
+let slowButton = document.getElementById("slowButton");
+let slowLight = document.getElementById("slowLight");
+
+let goButton = document.getElementById("goButton");
+let goLight = document.getElementById("goLight");
+
+stopButton.addEventListener("click", function (e) {
+	stopLight.classList.toggle("stop");
+	if (stopLight.classList.contains("stop")) {
+		console.log(`${e.target.textContent} bulb on`);
+	} else {
+		console.log(`${e.target.textContent} bulb off`);
+	}
+});
+slowButton.addEventListener("click", function (e) {
+	slowLight.classList.toggle("slow");
+	if (slowLight.classList.contains("slow")) {
+		console.log(`${e.target.textContent} bulb on`);
+	} else {
+		console.log(`${e.target.textContent} bulb off`);
+	}
+});
+
+goButton.addEventListener("click", function (e) {
+	goLight.classList.toggle("go");
+	if (goLight.classList.contains("go")) {
+		console.log(`${e.target.textContent} bulb on`);
+	} else {
+		console.log(`${e.target.textContent} bulb off`);
+	}
+});
+
+function addMouseLeaveListener(element) {
+	element.addEventListener("mouseleave", function (e) {
+		console.log(`Left ${e.target.textContent} button`);
+	});
+}
+
+function addMouseEnterListener(element) {
+	element.addEventListener("mouseenter", function (e) {
+		console.log(`Entered ${e.target.textContent} button`);
+	});
+}
+
+addMouseEnterListener(goButton);
+addMouseEnterListener(slowButton);
+addMouseEnterListener(stopButton);
+
+addMouseLeaveListener(goButton);
+addMouseLeaveListener(slowButton);
+addMouseLeaveListener(stopButton);
