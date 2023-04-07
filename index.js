03e7f9fe27031de7f3dcc57ea4a016c2651a7bc4
John Kluse <j.kluse@gmail.com>
2023-04-07 07:44:47 -0600

    Completed Stoplightw/Bonus

--- index.js
+++ index.js
@@ -6,31 +6,18 @@ let slowLight = document.getElementById("slowLight");
 
 let goButton = document.getElementById("goButton");
 let goLight = document.getElementById("goLight");
+let controls = document.getElementById("controls");
 
 stopButton.addEventListener("click", function (e) {
 	stopLight.classList.toggle("stop");
-	if (stopLight.classList.contains("stop")) {
-		console.log(`${e.target.textContent} bulb on`);
-	} else {
-		console.log(`${e.target.textContent} bulb off`);
-	}
 });
+
 slowButton.addEventListener("click", function (e) {
 	slowLight.classList.toggle("slow");
-	if (slowLight.classList.contains("slow")) {
-		console.log(`${e.target.textContent} bulb on`);
-	} else {
-		console.log(`${e.target.textContent} bulb off`);
-	}
 });
 
 goButton.addEventListener("click", function (e) {
 	goLight.classList.toggle("go");
-	if (goLight.classList.contains("go")) {
-		console.log(`${e.target.textContent} bulb on`);
-	} else {
-		console.log(`${e.target.textContent} bulb off`);
-	}
 });
 
 function addMouseLeaveListener(element) {
@@ -52,3 +39,31 @@ addMouseEnterListener(stopButton);
 addMouseLeaveListener(goButton);
 addMouseLeaveListener(slowButton);
 addMouseLeaveListener(stopButton);
+
+// bonus
+
+controls.addEventListener("click", function (e) {
+	if (e.target.textContent === "Stop") {
+		if (stopLight.classList.contains("stop")) {
+			console.log(`${e.target.textContent} bulb on`);
+		} else {
+			console.log(`${e.target.textContent} bulb off`);
+		}
+	}
+
+	if (e.target.textContent === "Slow") {
+		if (slowLight.classList.contains("slow")) {
+			console.log(`${e.target.textContent} bulb on`);
+		} else {
+			console.log(`${e.target.textContent} bulb off`);
+		}
+	}
+
+	if (e.target.textContent === "Go") {
+		if (goLight.classList.contains("go")) {
+			console.log(`${e.target.textContent} bulb on`);
+		} else {
+			console.log(`${e.target.textContent} bulb off`);
+		}
+	}
+});
